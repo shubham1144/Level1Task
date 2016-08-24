39a7e7babf0072b134a53478fb572b9b87f4921e
shubhamc <shubh.chodankar@gmail.com>
2016-08-24 06:53:26 +0530

    Adds client handling capability to the server

--- socketServer.js
+++ socketServer.js
@@ -2,10 +2,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var PORT = 3000;
+var io = require('socket.io')(http);
 
 //Create a function to handle all the incoming requests
 app.get('/', function(request, response){
 	response.send('Hello Client');
+});
+
+//On Connection logic for clients
+io.on('connection', function(clientSocket){
+	console.log('Client connected...');
+	clientSocket.on('disconnect', function(){
+		console.log('Client has been disconnected');
+	});
 });
 
 //The http server created should listen on a port for clients

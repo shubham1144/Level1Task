b5257b6bd43e371be74a5c7933e05462e9bc2060
shubhamc <shubh.chodankar@gmail.com>
2016-08-24 07:35:50 +0530

    Adds server side capability to secure communication using https

--- socketServer.js
+++ socketServer.js
@@ -1,8 +1,16 @@
 //Need to create a simple server using node.js and socket.io
 var app = require('express')();
-var http = require('http').Server(app);
+var https_options = {
+	key : key,
+	cert : cert
+};
+var https = require('https').createServer(https_options, app);
 var PORT = 3000;
-var io = require('socket.io')(http);
+var io = require('socket.io')(https);
+var fs = require('fs');
+//specify the self-signed public certificate and the private key used
+var key = fs.readFileSync('./private-key.pem');
+var cert = fs.readFileSync('./public-cert.pem');
 
 //Create a function to handle all the incoming requests
 app.get('/', function(request, response){
@@ -18,6 +26,6 @@ io.on('connection', function(clientSocket){
 });
 
 //The http server created should listen on a port for clients
-http.listen(PORT, function(){
+https.listen(PORT, function(){
 	console.log('Listening on port : ' + PORT );
 });

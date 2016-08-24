fabe2cad755cbc406286b820ea7ea2a9be88988e
shubhamc <shubh.chodankar@gmail.com>
2016-08-24 09:13:57 +0530

    Successfully authenticated client against server using the server's public key
    * the connection established between the two is tls/ssl using a self signed key

--- socketClient.js
+++ socketClient.js
@@ -1,7 +1,32 @@
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:3000');
+//var io = require('socket.io-client');
+//var socket = io.connect('http://localhost:3000');
+var HOST = 'localhost';
+var tls = require('tls');
+var fs = require('fs');
+
+var options = { 
+	ca  : [ fs.readFileSync('./public-cert.pem')]
+};
+
+console.log('Trying to establish a secure connection with the server');
+
+//Add a connection listener for the client over tls/ssl connection establishment
+var socketClient = tls.connect(3000, HOST, options, function(){
+	console.log('Trying to connect to socketServer hosted on port 3000');
+	if(socketClient.authorized){
+        console.log('Client has been authenticated successfully');
+	}else{
+		console.log('Client unauthenticated.. possible security breach');
+	}
+});
+
+socketClient.setEncoding('utf8');
+socketClient.on('data', function(data) {
+    console.log('-------------');
+    console.log(data);
+});
 
 //Add a connect listener for the client 
-socket.on('connect', function(socketClient){
-	console.log('Connecting to socket Server');
-});
+//socket.on('connect', function(socketClient){
+//	console.log('Connecting to socket Server');
+//});
--- socketServer.js
+++ socketServer.js
@@ -1,16 +1,17 @@
 //Need to create a simple server using node.js and socket.io
 var app = require('express')();
-var https_options = {
-	key : key,
-	cert : cert
-};
-var https = require('https').createServer(https_options, app);
 var PORT = 3000;
+var HOST = 'localhost';
 var io = require('socket.io')(https);
 var fs = require('fs');
 //specify the self-signed public certificate and the private key used
 var key = fs.readFileSync('./private-key.pem');
 var cert = fs.readFileSync('./public-cert.pem');
+var https_options = {
+	key : key,
+	cert : cert
+};
+var https = require('https').createServer(https_options, app);
 
 //Create a function to handle all the incoming requests
 app.get('/', function(request, response){
@@ -26,6 +27,5 @@ io.on('connection', function(clientSocket){
 });
 
 //The http server created should listen on a port for clients
-https.listen(PORT, function(){
-	console.log('Listening on port : ' + PORT );
-});
+https.listen(PORT, HOST);
+console.log('Server started on port : ' + PORT);

abdfc8726bdbf20d487c7ec9ee5a2a48ad9235db
shubhamc <shubh.chodankar@gmail.com>
2016-08-25 17:29:49 +0530

    Adds a tls server to handle requests from tls clients
    *uses tls to handle tls requests, and seperate https server to handle https requests

--- socketClient.js
+++ socketClient.js
@@ -2,35 +2,16 @@
 var HOST = 'localhost';
 var tls = require('tls');
 var fs = require('fs');
-//Adding the https module inorder tosend requests to server once a connection has been established
-var https = require("https");
-//client private key and public certificate
-var key = fs.readFileSync('./client-private-key.pem');
-var cert = fs.readFileSync('./client-public-cert.pem');
-
 //Options to be used when sending tls connection request to server
 var options = { 
 	ca  : [ fs.readFileSync('./public-cert.pem')]
 };
-//RequestOptions to be used used when sending https request to server
-var postOptions = {
-  hostname: 'localhost',
-  port: 3000,
-  path: '/sendstatistics',
-  method: 'POST',
-  headers: {
-      'Content-Type': 'application/json',
 
-  },
-	key: key,
-	cert: cert,
-	ca  : [ fs.readFileSync('./public-cert.pem')]
-};
 console.log('Trying to establish a secure connection with the server');
 	try{
 //Add a connection listener for the client over tls/ssl connection establishment
-var socketClient = tls.connect(3000, HOST, options, function(){
-	console.log('tls connection in progress...');
+var socketClient = tls.connect(8000, HOST, options, function(){
+	console.log('tls connection in progress');
 	if(socketClient.authorized){
 
 		console.log('CONNECTED OVER TLS/SSL WITH SELF SIGNED KEY AND AUTHORIZED\n');
@@ -43,21 +24,11 @@ var socketClient = tls.connect(3000, HOST, options, function(){
 
 		//Send statistical data in intervals
 		setInterval(function() { 
-
+            
 			console.log('Sending out readings every 10 seconds');
-			//sending a post request to server every 10 seconds
-            var req = https.request(postOptions, function(res) {
-			res.setEncoding('utf8');
-			res.on('data', function (body) {
-				console.log(body);
-				});
-				});
-			req.on('error', function(e) {
-				console.log('problem with request: ' + e.message);
-				});
-			//Write data to request body
-			req.write('{"Device_identity": "Device-001", "Latitude": 15.4499170, "Longitude": 73.826066,"Time": "051050","Date": "20160822", "Status": "0x0A", "Speed": 23}');
-			req.end();
+            //Need to send data through the socket itself
+            socketClient.write('{"Device_identity": "Device-' + socketClient.localPort + '","Latitude": 15.4499170, "Longitude": 73.826066,"Time": "051050","Date": "20160822", "Status": "0x0A", "Speed": 23}');
+
 
 		}, 10000);
 	    //the logic to send data in interval ends here
@@ -71,3 +42,13 @@ var socketClient = tls.connect(3000, HOST, options, function(){
 	catch(ex){
       console.log('Error occured due to : ' + ex + 'Exiting the client');
 	}
+
+//Receive tls data from the server
+socketClient.on("data", function (data) {
+  console.log('echo Server');
+});	
+//Error handler for socket connectoin established
+socketClient.on('error', function (err) {
+  console.log("Error Occured due to  :  " + err);
+  process.exit();
+});	
--- socketServer.js
+++ socketServer.js
@@ -1,14 +1,16 @@
 //Create a server using express.
 var app = require('express')();
 var PORT = 3000;
+var TLS_PORT = 8000;
 var HOST = 'localhost';
+var tls = require('tls');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 //specify the self-signed public certificate and the private key used
 var key = fs.readFileSync('./private-key.pem');
 var cert = fs.readFileSync('./public-cert.pem');
-var https_options = {
+var options = {
 	key : key,
 	cert : cert,
 	ca  : [ fs.readFileSync('./client-public-cert.pem')]
@@ -16,14 +18,6 @@ var https_options = {
 //Adding a library to convert ist timestamp to unix timestamp
 var moment = require('moment');
 moment().format();
-
-//Create a secured https server using the options
-var https = require('https').createServer(https_options, app);
-
-//We require body parser to get json data from post requests
-app.use(bodyParser.urlencoded({ extended: true }));
-//Inorder for the request body to be parsed by the post method we need to use the below line 
-app.use(bodyParser.json());
 
 //connect to mongodb instance
 mongoose.connect('mongodb://127.0.0.1:27017/devicestatistics');
@@ -48,38 +42,54 @@ var DeviceTrack = new Schema({
 
 var DeviceTrack = mongoose.model('DeviceTrack', DeviceTrack);     
 
-//Create a function to handle all the incoming requests
-app.get('/', function(request, response){
-	console.log('Landing page request caught');
-	
-});
+//Create a secured https server using the options for level 2 tasks
+var https = require('https').createServer(options, app);
 
-//Create a handler for post request , to receive data from the client socket created using tls sockets.
-app.post('/sendstatistics', function(request, response) {
-    console.log('Device Statistics received ...');
-	request.body.Timestamp = generateUnixTimeStamp(request.body.Date, request.body.Time);
+//Create a tls server using the options for level 1 task
+var tlsserver = tls.createServer(options, function (serverSocket) {
+
+  serverSocket.write("Echo device..." + "\n");
+  serverSocket.pipe(serverSocket);
+  //Add a listener for client data being received
+  serverSocket.addListener("data", function (data) {
+	console.log('Device Statistics received from :  Device-' + serverSocket.remotePort);
+	var jsondataObject = JSON.parse(data);
+	jsondataObject.Timestamp = generateUnixTimeStamp(jsondataObject.Date, jsondataObject.Time);
+
 	//Insert the data sent by client into our mongodb database
-	var deviceTrack = new DeviceTrack(request.body);
-
+	var deviceTrack = new DeviceTrack(jsondataObject);
+         
 	        deviceTrack.save( function(error, data){
 	            if(error){
 	            	console.log('Error occured while saving in db ' + error);
-	                response.json(error);
 	            }
 	            else{
 	            	console.log('Storing in db..');
 	            }
 	        });
-	response.write('Data received by server..');
-    
+    });
+
+ 
+}).listen(TLS_PORT);
+
+
+//We require body parser to get json data from https post requests
+app.use(bodyParser.urlencoded({ extended: true }));
+//Inorder for the request body to be parsed by the https post method we need to use the below line 
+app.use(bodyParser.json());
+
+//Create a function to handle all the incoming https requests(Level 2 tasks)
+app.get('/', function(request, response){
+	console.log('Landing page request caught');
+	
 });
 
+//The http server created should listen on a port for clients
+https.listen(PORT, HOST);
 //Function to return unix timestamp from the IST date and time
 function generateUnixTimeStamp(Date, Time){
 
 	return  moment(Date+Time).unix();
 
 };
-//The http server created should listen on a port for clients
-https.listen(PORT, HOST);
 console.log('Server is runnning on port : ' + PORT );
